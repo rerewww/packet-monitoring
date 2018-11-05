d0e9175427cf0d9b7e8ba967f333dc2333bf7252
rerewww <rerewww@naver.com>
2018-11-06 01:30:10 +0900

    master-1081030 - 구글 차트 사용 - 패킷들 JSON Array list로 받도록 수정 - 자바스크립트에서 파싱하여 결과를 구글 차트 데이터에 사용

--- src/main/java/Network/PacketContainer.java
+++ src/main/java/Network/PacketContainer.java
@@ -3,17 +3,26 @@ package Network;
 import Config.Status;
 import lombok.Getter;
 import lombok.Setter;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by son on 2018-10-28.
  */
 public class PacketContainer {
     public List<Packet> packets = new ArrayList<>();
+    public Map<String, Integer> packetsMb = new HashMap<>();
     @Getter @Setter private int totalListen;
     @Getter @Setter private int totalConnect;
+
+    public void sendRevicePacketMb(final String line) {
+        // 받은, 보낸 패킷 파싱
+    }
 
     public void setTotalCount() {
         int cntListen = 0;
@@ -72,12 +81,16 @@ public class PacketContainer {
         packets.add(packet);
     }
 
-    public StringBuffer print() {
-        StringBuffer buffer = new StringBuffer();
+    public JSONArray print() {
+        JSONArray jsonArray = new JSONArray();
+
         for (Packet packet : packets) {
-            buffer.append(packet.print());
-            buffer.append("\n");
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("protocol", packet.getProtocol());
+            jsonObject.put("status", packet.getStatus());
+            jsonArray.put(jsonObject);
         }
-        return buffer;
+
+        return jsonArray;
     }
 }
