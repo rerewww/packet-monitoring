86742549b8c2145eb3af9f4c7c947c5336425650
rerewww <rerewww@naver.com>
2018-11-30 23:48:37 +0900

    master-181130/Add IpModel class

--- src/main/java/Network/model/TcpModel.java
+++ src/main/java/Network/model/TcpModel.java
@@ -1,10 +1,12 @@
 package Network.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
  * Created by son on 2018-11-28.
  */
+@AllArgsConstructor
 public class TcpModel {
     @Getter private final int source;
     @Getter private final int destination;
@@ -14,14 +16,4 @@ public class TcpModel {
     @Getter private final int window;
     @Getter private final int urgent;
     @Getter private final String flag;
-public TcpModel(final int source, final int destination, final long seqNumber, final long acknowledgeNumber, final int offset, final int window, final int urgent, final String flag) {
-        this.source = source;
-        this.destination = destination;
-        this.seqNumber = seqNumber;
-        this.acknowledgeNumber = acknowledgeNumber;
-        this.offset = offset;
-        this.window = window;
-        this.urgent = urgent;
-        this.flag = flag;
-    }
 }
